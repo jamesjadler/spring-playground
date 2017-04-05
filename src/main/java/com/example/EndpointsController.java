8d3f6bb589ba06d8dbf649ab8da9477e1573ae84
trainer7 <trainer7@trainer7s-MacBook-Pro.local>
2017-04-04 18:20:29 -0600

    Unit 3 Exercise 1 get pi

--- src/main/java/com/example/EndpointsController.java
+++ src/main/java/com/example/EndpointsController.java
@@ -1,14 +1,14 @@
 package com.example;
 
+
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-public class HelloController {
+public class EndpointsController {
 
-    @GetMapping("/")
-    public String helloWorld() {
-        return "Hello from Spring!";
+    @GetMapping("/math/pi")
+    public String getIndex() {
+        return Double.toString(Math.PI);
     }
-
 }
